059e1f7742b0c6a1865430716ef68cf118ff549e
Miguel <miguel.viloria.247@gmail.com>
2023-02-22 12:25:27 -0300

    cors

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 
 //Config express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// cors config
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    allowedHeaders: 'Content-Type,Authorization'
+}
+
+app.use(cors(corsOptions))
+
+
 
 app.use(require('./routes'))
+
 
 app.get('/', (req, res) => {
     res.send('All Ok');
